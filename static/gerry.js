16b990da286599bab2092dcc3a0293b96623f64c
Will Crumbley <wcrumbley@kyruus.com>
2017-07-08 15:08:27 -0400

    add presidential data and impute; update styling and display (accidentally unfocused commit)

--- static/gerry.js
+++ static/gerry.js
@@ -15,15 +15,31 @@ var total_state_votes = 0;
 var dem_wasted_votes = 0;
 var rep_wasted_votes = 0;
 for (let district of state.house_districts) {
-    var total_votes = district.dem_votes + district.rep_votes;
+    for (let year of ["y2016", "y2014", "y2012"]) {
+        for (let party of ["dem", "rep"]) {
+            if (district.votes[year][party + "_votes_house"] === null) {
+                if (year === "y2014") {
+                    district.votes[year][party + "_votes_house"] = district.votes.y2012[party + "_votes_pres"]
+                }
+                else {
+                    district.votes[year][party + "_votes_house"] = district.votes[year][party + "_votes_pres"]
+                }
+            }
+        }
+    }
+    var dem_votes_avg = (district.votes.y2016.dem_votes_house + district.votes.y2014.dem_votes_house + 
+                             district.votes.y2012.dem_votes_house)/3
+    var rep_votes_avg = (district.votes.y2016.rep_votes_house + district.votes.y2014.rep_votes_house + 
+                             district.votes.y2012.rep_votes_house)/3
+    var total_votes = dem_votes_avg + rep_votes_avg;
     total_state_votes += total_votes;
-    if (district.dem_votes > district.rep_votes) {
-        dem_wasted_votes += (district.dem_votes - total_votes / 2);
-        rep_wasted_votes += district.rep_votes;
+    if (dem_votes_avg > rep_votes_avg) {
+        dem_wasted_votes += (dem_votes_avg - total_votes / 2);
+        rep_wasted_votes += rep_votes_avg;
     }
     else {
-        rep_wasted_votes += (district.rep_votes - total_votes / 2);
-        dem_wasted_votes += district.dem_votes;
+        rep_wasted_votes += (rep_votes_avg - total_votes / 2);
+        dem_wasted_votes += dem_votes_avg;
     }
 };
 include = state.house_districts.length >= 8 ? true : false;
@@ -67,6 +83,12 @@ gerry_app.display_state_metrics = function(states) {
         var metric = "<td>" + state.metric + "</td>";
         var seats_flipped = "<td>" + state.seats_flipped + "</td>";
         var districts = "<td>" + state.house_districts.length + "</td>";
+        var num_dem_seats = state.house_districts
+            .filter(district => district.votes.y2016.dem_votes_house > district.votes.y2016.rep_votes_house);
+        var num_rep_seats = state.house_districts
+            .filter(district => district.votes.y2016.rep_votes_house > district.votes.y2016.dem_votes_house);
+        var dem_seats = "<td>" + num_dem_seats + "</td>";
+        var rep_seats = "<td>" + num_rep_seats + "</td>";
         var map_image = "<img class='state-map' src='" + map_url_prefix + state.code.toLowerCase() + ".gif'>";
         var map = "<td><a href = 'https://nationalmap.gov/small_scale/printable/congress.html' target='_blank'>" + 
             map_image + "</a></td>";
@@ -118,12 +140,16 @@ gerry_app.set_metric_function = function() {
     $('#metric-function').val(metric_function);    
 }
 
+gerry_app.display_input_data = function(state_data) {
+    $("#state-data").val(state_data);
+}
+
 $(function() {
     gerry_app.set_metric_function();
     $.getJSON("../data/house_by_state.json", function(house_json) {
         gerry_app.house_json = house_json;
         var calculate_button = $('#calculate-metric');
         calculate_button.click(gerry_app.calculate_metrics);
-        calculate_button.click();
+        gerry_app.display_input_data(house_json.states)
     });
 });
