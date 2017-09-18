4d6064f3c4d4812c0646f72c3ec344d17ae32503
Paavan Bhavsar <pbhavsar@kyruus.com>
2017-09-18 00:07:44 -0400

    Remove rendering of narrative modal

--- static/components/navigation.jsx
+++ static/components/navigation.jsx
@@ -21,10 +21,7 @@ class Navigation extends React.Component {
     render() {
         return (
             <div>
-                <div id='modal'>
-                    <NarrativeModal show={true} states={this.props.states} onCloseModal={this.showNavbar}/>
-                </div>
-                <div id='nav' hidden={this.state.hideNav} className='hidden-sm-down'>
+                <div id='nav' className='hidden-sm-down'>
                     <nav className="navbar fixed-top navbar-toggleable-md navbar-light bg-faded">
                       <a className="navbar-brand" href="#">
                         <img src='./static/img/gerrymander-500.png' width="30" height="30" 
