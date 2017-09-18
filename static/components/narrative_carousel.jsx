c55c1206736bd19b6d84e7ea60ebc785ef3b1092
Paavan Bhavsar <pbhavsar@kyruus.com>
2017-09-18 00:08:01 -0400

    Add narrative carousel component

---
+++ static/components/narrative_carousel.jsx
@@ -0,0 +1,16 @@
+"use strict"
+
+import React from 'react';
+import ReactModal from 'react-modal';
+import $ from 'jquery';
+
+import narrative_data from './narrative.jsx';
+
+function NarrativeCarousel(props) {
+  return (
+    <div class="carousel slide" data-ride="carousel">
+    </div>
+  )
+};
+
+module.exports = NarrativeCarousel;
