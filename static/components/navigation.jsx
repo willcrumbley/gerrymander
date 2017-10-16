dcb2b0e808e0bad20d1b5c4093d52b69037ef257
Paavan Bhavsar <pbhavsar@kyruus.com>
2017-10-16 01:11:22 -0400

    Fix navbar

--- static/components/navigation.jsx
+++ static/components/navigation.jsx
@@ -23,16 +23,18 @@ class Navigation extends React.Component {
             <div>
                 <div id='navigation'>
                     <nav className="navbar fixed-top navbar-toggleable-md navbar-light bg-faded">
+                      <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                      </button>
                       <a className="navbar-brand" href="#">
                         <img src='./static/img/gerrymander-500.png' width="30" height="30" 
                             alt="Original 1812 Gerrymander cartoon"/>
                       </a>
                       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/introduction">Introduction</Link>
-                            </li>
-                        </ul>
+                        <div className="navbar-nav">
+                          <Link className="nav-item nav-link" to="/">Home</Link>
+                          <Link className="nav-item nav-link" to="/introduction">Introduction</Link>
+                        </div>
                       </div>
                     </nav>
                 </div>
