77e6616254341a2e49de9db5c32668a6d08d941e
Paavan Bhavsar <pbhavsar@kyruus.com>
2017-10-28 15:56:28 -0400

    Revert "Simplify default efficiency gap algorithm, only calculating 2016 house election"
    This reverts commit efb1a2a4f02e624c5a827336ea73bf7dbba24e02.

--- static/efficiency_gap_ge_8.js
+++ static/efficiency_gap_ge_8.js
@@ -3,42 +3,51 @@
 module.exports = `/**
  * Calculating the efficiency gap for the 2016 House of Representatives Election
  */
-
 var state = options.state;
-var state_total_votes = 0;
-var d_wasted_votes = 0;
-var r_wasted_votes = 0;
+var state_total = 0;
+var d_wasted = 0;
+var r_wasted = 0;
 
-for (let district of state.house_districts) {
-  let d_votes = district.votes.y2016.d_votes_house;
-  let r_votes = district.votes.y2016.r_votes_house;
-  let other_votes = district.votes.y2016.other_votes_house;
+for (let dist of state.house_districts) {
+  // Impute data from Presidential races for uncontested elections
+  for (let year of ["y2016", "y2014", "y2012"]) {
+    for (let party of ["dem", "rep"]) {
+      if (dist.votes[year][party + "_votes_house"] === null) {
+        if (year === "y2014") {
+          dist.votes[year][party + "_votes_house"] = dist.votes.y2012[party + "_votes_pres"]
+        }
+        else {
+          dist.votes[year][party + "_votes_house"] = dist.votes[year][party + "_votes_pres"]
+        }
+      }
+    }
+  }
 
-  var total_votes = d_votes + r_votes + other_votes;
-  var minimum_to_win = total_votes / 2 + 1;
+  // Average votes
+  var d_vote_avg = (dist.votes.y2016.d_votes_house + dist.votes.y2014.d_votes_house + 
+    dist.votes.y2012.d_votes_house)/3
+  var r_vote_avg = (dist.votes.y2016.r_votes_house + dist.votes.y2014.r_votes_house + 
+    dist.votes.y2012.r_votes_house)/3
 
   // Calculate wasted votes
-  if (d_votes > r_votes) {
-    d_wasted_votes += d_votes - minimum_to_win;
-    r_wasted_votes += r_votes;
+  var total_votes = d_vote_avg + r_vote_avg;
+  state_total += total_votes;
+  if (d_vote_avg > r_vote_avg) {
+    d_wasted += (d_vote_avg - total_votes / 2);
+    r_wasted += r_vote_avg;
   }
   else {
-    d_wasted_votes += d_votes;
-    r_wasted_votes += r_votes - minimum_to_win;
+    r_wasted += (r_vote_avg - total_votes / 2);
+    d_wasted += d_vote_avg;
   }
-
-  // Keep a running total of the total votes cast
-  state_total_votes += total_votes;
-}
+};
 
 // Calculate the efficiency gap
-var metric = (r_wasted_votes - d_wasted_votes) / state_total_votes;
-
-// Only include if there are at least 8 districts
-var include = state.house_districts.length >= 8;
-
+var include = state.house_districts.length >= 8 ? true : false;
+var metric = (r_wasted - d_wasted) / state_total;
 return {
  metric: metric,
  include: include,
  seats_flipped: metric * state.house_districts.length
-}`
+}
+`
